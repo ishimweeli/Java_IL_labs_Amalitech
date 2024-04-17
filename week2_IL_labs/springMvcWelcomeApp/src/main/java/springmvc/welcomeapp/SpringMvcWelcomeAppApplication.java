2f2efc0204106bab5af54bcb1b22e50160a3ef4a
ISHIMWE ELIAB <97574791+ishimweeli@users.noreply.github.com>
2024-04-17 18:48:17 +0200

    first commit

---
+++ week2_IL_labs/springMvcWelcomeApp/src/main/java/springmvc/welcomeapp/SpringMvcWelcomeAppApplication.java
@@ -0,0 +1,13 @@
+package springmvc.welcomeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcWelcomeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcWelcomeAppApplication.class, args);
+	}
+
+}
---
+++ week2_IL_labs/springMvcWelcomeApp/src/test/java/springmvc/welcomeapp/SpringMvcWelcomeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package springmvc.welcomeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcWelcomeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
