2f8a8013a008af437a0536debab89912eda6e4a7
ISHIMWE ELIAB <97574791+ishimweeli@users.noreply.github.com>
2024-05-02 10:40:13 +0200

    updates

--- personalExercises/ThreadSafeCache.java
+++ personalExercises/ThreadSafeCache.java
@@ -1,22 +1,27 @@
-//import java.util.Map;
-//import java.util.concurrent.ConcurrentHashMap;
-//
-//public class ThreadSafeCache<T,V> {
-//
-//    private final Map<K,v> cache;
-//
-//    public ThreadSafeCache(){
-//        cache=new ConcurrentHashMap<>()
-//    }
-//    public void put(K key, V value){
-//        cache.put(key,value)
-//    }
-//    public V get (K key){
-//        cache.get(key)
-//    }
-//
-//    public void remove(K key){
-//        cache.remove(key)
-//    }
-//
-//}
+package personalExercises;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ThreadSafeCache<K,V> {
+
+    private final Map<K,V> cache;
+
+    public ThreadSafeCache(){
+        cache=new ConcurrentHashMap<>();
+    }
+    public void put(K key, V value){
+        cache.put(key,value);
+    }
+    public V get (K key){
+       return cache.get(key);
+
+    }
+
+    public void remove(K key){
+        cache.remove(key);
+    }
+
+
+}
