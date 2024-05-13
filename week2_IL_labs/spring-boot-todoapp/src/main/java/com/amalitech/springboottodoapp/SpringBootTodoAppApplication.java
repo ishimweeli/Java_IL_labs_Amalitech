e7702b1e64ea8a2014c7a024bd1583f8f53395ee
ISHIMWE ELIAB <97574791+ishimweeli@users.noreply.github.com>
2024-05-13 09:16:17 +0200

    to do app fix

--- week2_IL_labs/spring-boot-todoapp/src/main/java/com/amalitech/springboottodoapp/SpringBootTodoAppApplication.java
+++ week2_IL_labs/spring-boot-todoapp/src/main/java/com/amalitech/springboottodoapp/SpringBootTodoAppApplication.java
@@ -13,11 +13,4 @@ public class SpringBootTodoAppApplication {
         SpringApplication.run(SpringBootTodoAppApplication.class, args);
     }
 
-//    @Bean
-//    public ViewResolver viewResolver() {
-//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//        viewResolver.setPrefix("classpath:/templates/");
-//        viewResolver.setSuffix(".html");
-//        return viewResolver;
-//    }
 }
